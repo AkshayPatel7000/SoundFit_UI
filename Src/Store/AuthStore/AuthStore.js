f76611458322a3af9d37356509e4a68ed16cb9f5
akshay <akshay.patel@moreyeahs.co>
2023-01-12 19:31:55 +0530

    mobx

--- Src/Store/AuthStore/AuthStore.js
+++ Src/Store/AuthStore/AuthStore.js
@@ -3,16 +3,20 @@ import {action, makeObservable, observable} from 'mobx';
 class AuthStore {
   userData = {
     actionSheet: false,
+    steps: {},
   };
   constructor() {
     makeObservable(this, {
       userData: observable,
       setActionSheet: action,
+      setSteps: action,
     });
   }
   setActionSheet(value) {
     this.userData.actionSheet = value;
   }
-  
+  setSteps(value) {
+    this.userData.steps = value;
+  }
 }
 export const authStore = new AuthStore();
